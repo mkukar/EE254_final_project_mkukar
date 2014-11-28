c8750b5c0f358396c28072efd9f6c93062aa0c68
Michael Kukar <mkukar@gmail.com>
2014-11-27 23:00:52 -0800

    Lots of changes for player test

--- finalProject.v
+++ finalProject.v
@@ -18,13 +18,16 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module helicopter_game(ClkPort, btnC, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, vgaRed1, vgaRed2, vgaGreen1, vgaGreen2, vgaBlue2
+module helicopter_game(ClkPort, btnC, btnL, btnR, btnU, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, vgaRed1, vgaRed2, vgaGreen1, vgaGreen2, vgaBlue2
     );
 
 //inputs
 
 input ClkPort;
 input btnC;
+input btnL;
+input btnR;
+input btnU;
 
 //output
 
@@ -45,10 +48,11 @@ reg [9:0] highScore;
 reg [9:0] playerLoc;
 reg hsFlag;
 reg collision;
-reg playerLength = 640; //MAX VALUE FOR PLAYER LOCATION (640 for 1 px player size)
 
 assign reset = btnC;
-assign startButton = btnL; //ASSIGNS THE START BUTTON TO THE LEFT BUTTON, HAVE TO CHANGE OTHER FILE
+assign startButton = btnU; //ASSIGNS THE START BUTTON TO THE UP BUTTON, HAVE TO CHANGE OTHER FILE
+assign leftButton  = btnL;
+assign rightButton = btnR;
 
 always @ (posedge ClkPort, posedge reset)
 	begin
@@ -81,7 +85,10 @@ always @ (posedge vga_clk)
 		vgaBlue2  <= white & inDisplayArea;
 	end
 
-wire white = (CounterY >=100&&CounterY<=200) && (CounterX >= 100 && CounterX <= 200);
+//playerIcon is 10 px wide and is NOT centered around the playerLoc but instead that is the leftmost location
+wire playerIcon = (CounterY >= 460 && CounterY <= 470) && (CounterX >= playerLoc && CounterX <= (playerLoc + 10));
+
+wire white = playerIcon;
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -99,11 +106,11 @@ localparam
 		DEAD   = 3’b011, //dead state (possibly unneeded)
 		UNK    = 3’bXXX; //unclear state for everything else
 
-always @ (posedge sys_clk, posedge reset) begin: STATE_MACHINE
+always @ (posedge vga_clk, posedge reset) begin: STATE_MACHINE
 
 	if (reset) begin
 		//CODE FOR RESETTING GOES HERE, CENTER THE PLAYER, CLEAR SCORE, ETC.
-
+		playerLoc <= 315;
 		state <= INIT;
 	end
 
@@ -117,19 +124,26 @@ always @ (posedge sys_clk, posedge reset) begin: STATE_MACHINE
 
 				//rtl logic
 				playerScore <= 0;
-				playerLoc <= playerLength/2;
+				playerLoc <= 315;
 			end
 
 			PLAY: begin
 				//state transitions
+
+				
+				//rtl logic
+				if (rightButton) begin
+					playerLoc <= playerLoc + 5;
+				end
+				else if (leftButton) begin
+					playerLoc <= playerLoc - 5;
+				end
 				if (collision == 1 && hsFlag == 1) begin
 					state <= NEWHS;
 				end
 				else if (collision == 1 && hsFlag == 0) begin
 					state <= DEAD;
 				end
-				//rtl logic
-				
 			end
 
 			NEWHS: begin
